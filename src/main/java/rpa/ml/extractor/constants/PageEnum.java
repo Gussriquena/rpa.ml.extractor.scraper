1e5ecb55afc1e3dd562ff70300c4085c9a2346a7
Gustavo Riquena <gustavo_riquena1@outlook.com>
2021-03-08 15:36:27 -0300

    properties creation, configEnum, PageEnum and ConfigReader, all page constants

--- src/main/java/rpa/ml/extractor/constants/PageEnum.java
+++ src/main/java/rpa/ml/extractor/constants/PageEnum.java
@@ -1,5 +1,27 @@
 package rpa.ml.extractor.constants;
 
-public enum PageEnum {
+import rpa.ml.extractor.utils.ConfigReader;
 
+public enum PageEnum {
+	
+	// URLs from properties
+	URL_ML_MAIN(ConfigReader.read("ml.url.page.main")),
+	URL_ML_SEARCH(ConfigReader.read("ml.url.page.search")),
+	
+	// Elements
+	XPATH_PRODUCT_LIST_ITEM(ConfigReader.read("//li[@class='ui-search-layout__item']")),
+	XPATH_PRODUCT_LIST_LINK(ConfigReader.read("//li[@class='ui-search-layout__item']//a[1][@class='ui-search-link']")),
+	XPATH_PRODUCT_PAGE_PRICE(ConfigReader.read("(//div[@class='ui-pdp-price__second-line']/span/span[@class='price-tag-fraction'])[1]")),
+	XPATH_PRODUCT_PAGE_AMOUNT(ConfigReader.read("//div[@class='ui-pdp-header']/div/span[@class='ui-pdp-subtitle']"));
+	
+	private String value;
+	
+	private PageEnum(String description) {
+		this.value = description;
+	}
+	
+	private String getValue() {
+		return value;
+	}
+	
 }
